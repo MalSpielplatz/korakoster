3fc3f2f9037d18d8eafc6cd3c6a9d20eeb211073
Mohammad Ikmal <111510512+MalSpielplatz@users.noreply.github.com>
2022-08-23 02:21:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print ("---------------- Selamat datang di koracoster ----------------")
+tinggi = int(input("Berapa tinggi kamu dalam cm ? "))
+
+if tinggi >= 120:
+  print ("Silahkan, Kamu boleh naik koracoster XD")
+else:
+  print("maaf, kamu tidak boleh naik koracoster, tinggi kamu harus lebih dari 120 cm")
